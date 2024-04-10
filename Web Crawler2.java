9168f596814090f01b4215b494d54ad96c364d1f
tamelon <38846671+tamelon@users.noreply.github.com>
2024-04-09 22:22:18 -0400

    Update Web Crawler2.java

--- Web Crawler2.java
+++ Web Crawler2.java
@@ -8,24 +8,28 @@ import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class WebCrawler {
+public class Webcrawler {
     private Set<String> visitedUrls;
     private String startingUrl;
     private int maxDepth;
-    private int numThreads;
     private ExecutorService executor;
 
-    public WebCrawler(String startingUrl, int maxDepth, int numThreads) {
+    public Webcrawler(String startingUrl, int maxDepth, int numThreads) {
         this.visitedUrls = new HashSet<>();
         this.startingUrl = startingUrl;
         this.maxDepth = maxDepth;
-        this.numThreads = numThreads;
         this.executor = Executors.newFixedThreadPool(numThreads);
     }
 
     public void startCrawling() {
         crawl(startingUrl, 0);
         executor.shutdown();
+        while (!executor.isTerminated()) {
+            // Wait for all tasks to finish
+        }
+        // After all threads are finished, print visited URLs for example
+        System.out.println("Visited URLs:");
+        visitedUrls.forEach(System.out::println);
     }
 
     private void crawl(String url, int depth) {
@@ -37,18 +41,18 @@ public class WebCrawler {
 
         Runnable task = () -> {
             try {
-                // Fetch the HTML content of the page
                 Document document = Jsoup.connect(url).get();
                 processPage(document);
-
-                // Find and crawl links on the page
+                
                 Elements links = document.select("a[href]");
                 for (Element link : links) {
                     String nextUrl = link.absUrl("href");
-                    crawl(nextUrl, depth + 1);
+                    if (!nextUrl.isEmpty()) {
+                        crawl(nextUrl, depth + 1);
+                    }
                 }
             } catch (IOException e) {
-                e.printStackTrace();
+                System.err.println("Error fetching URL: " + url);
             }
         };
 
@@ -56,17 +60,24 @@ public class WebCrawler {
     }
 
     private void processPage(Document document) {
-        // Implement your logic to process the page here
-        // For example, you can extract data, save to a database, etc.
+        // Here you can implement logic to process the page
+        // For example, printing the title of the page
+        System.out.println("Page Title: " + document.title());
     }
-}
-public class Main {
+
     public static void main(String[] args) {
-        String startingUrl = "https://example.com";
+        String[] startingUrls = {
+            "http://naver.com",
+            "http://google.com",
+            "http://youtube.com",
+            "https://www.op.gg/"
+        };
         int maxDepth = 5;
         int numThreads = 4;
-
-        WebCrawler crawler = new WebCrawler(startingUrl, maxDepth, numThreads);
-        crawler.startCrawling();
+    
+        for (String url : startingUrls) {
+            Webcrawler crawler = new Webcrawler(url, maxDepth, numThreads);
+            crawler.startCrawling();
+        }
     }
 }
